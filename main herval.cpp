4d96fed61d57777d88ddf310fa8f570f879f3be5
Yulian Garcia <yuliangarcia-17@hotmail.com>
2017-03-05 14:34:19 -0500

    arrays terminado

--- main herval.cpp
+++ main herval.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cstring>
 #include <conio.h>
+#include <math.h>
 using namespace std;
 
 int seleccion, opcion, a, limite, suma, x, limite_2, suma_2, suma_3, y,suma_4, z,limite_3, limite_4, opcion_2, i, num, res, j, num_1, k, factorial;
-int e, b=0, c=1 ,d=1, l, vector [5], m, n, o, opcion_3;
+int e, b=0, c=1 ,d=1, l, vector [5], m, n, o, opcion_3, largo, ancho;
 
 int main(int argc, char** argv) 
 {
@@ -12,11 +13,11 @@ int main(int argc, char** argv)
 	
 system("CLS");
 cout<< "MENU PRINCIPAL\n\n";
-cout<< "presione 1 para acceder  las opciones del while\n";
+cout<< "presione 1 para acceder a las opciones del while\n";
 cout<< "presione 2 para acceder a las opciones del ciclo for\n";
 cout<< "presione 3 para acceder a las opciones del array\n";
 cout<< "presione 4 para acceder a las opciones del switch\n";
-cout<< "presione 5 para acceder ala modulo de matrices\n";
+cout<< "presione 5 para acceder al modulo de matrices\n";
 cout<< "presione 6 para acceder al modulo de estructuras\n\n";
 cout<< "SELECCIONE LA OPCION O EL MODULO DESEADO\n";
 cin>> seleccion;
@@ -198,8 +199,79 @@ switch (seleccion)
 					        cout<<"vector "<<dim<< " = "<<vector[dim]<<endl;
 				        	} 
 				        	break;
-				        	system("CLS");
-                    }break;
+				        
+							
+							// ARREGLO NUM1 X NUM 2 CON 0 Y 1
+							case 2:{
+								int  a=0, p, e;
+								cout<<"ingrese el largo de la matriz\n";
+								cin>>largo;
+								cout<<"ingrese el ancho de la matriz\n";
+								cin>>ancho;
+								system ("CLS");
+								int t [largo][ancho];
+								for ( p=0; p<largo; p++){
+									cout<<"\n";
+									
+									for (e=0; e<ancho; e++){
+										t [largo] [ancho] =a;
+										cout<< t[largo] [ancho];
+										
+										if (a==0){
+										a=1;
+										}else{
+										a=0;
+										}
+									}
+								}
+							}
+								break;
+								
+								// ARREGLO DIMENSION 4X4
+								case 3:
+									int i,j;
+									int matriz [4][4];
+									for (i=0; i<4; i++){
+										cout<<"ingrese el numero del vector "<<i+1<<endl;
+										cin>>matriz [0][i];
+									}
+									for (i=0; i<4; i++){	
+										cout<<"\n";
+										for (j=0; j<4; j++){
+											if(i==0)
+											{
+												cout<< matriz [i] [j]<<" " ;
+											}
+											
+											if(i==1)
+											{
+												matriz [i] [j]= pow(matriz[0][j],2);
+                                				cout<< matriz [i] [j]<<" " ;
+											}
+											
+											if(i==2)
+                            				{
+                                			matriz [i] [j]= pow(matriz[0][j],3) ;
+                                			cout<< matriz [i] [j]<<" " ;
+                            				}
+                            				
+                           					 if(i==3)
+                           					 {
+                             			    matriz [i] [j]= pow(matriz[0][j],4) ;
+                              			    cout<< matriz [i] [j]<<" ";
+											}
+										}
+									}
+										break;	
+										
+										default:
+										cout<<"\nopcion no valida\n";
+										break;
+										system ("CLS");
+					}break;
+										
+										
+											
 }getch();
 }while(seleccion>0);
 	
